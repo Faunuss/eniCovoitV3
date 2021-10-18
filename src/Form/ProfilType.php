b40f99cb644ea385bebe8d9228fa7a791b336a81
Faunuss <janitor.franck@gmail.com>
2021-10-18 10:56:46 +0200

    afficher/modifier profil

---
+++ src/Form/ProfilType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfilType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pseudo')
+            ->add('nom')
+            ->add('prenom')
+            ->add('telephone')
+            ->add('email')
+            ->add('new_password', RepeatedType::class, [
+                'required'=>false,
+                'type' => PasswordType::class,
+                "mapped"=>false,
+                'invalid_message' => 'Les mots de passe doivent correspondre.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'first_options'  => ['label' => 'Nouveau mot de passe'],
+                'second_options' => ['label' => 'Confirmation'],
+            ])
+            ->add('plainPassword', PasswordType::class,["mapped"=>false, 'label' => 'Mot de passe'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
